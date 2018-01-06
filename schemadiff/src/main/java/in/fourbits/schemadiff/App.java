2e67a0f1f910a8abc2e75d20f3eff367c7e12532
C5260818 <suraj.trivikram.acharya@sap.com>
2018-01-07 00:53:37 +0530

    Added email notification

--- schemadiff/src/main/java/in/fourbits/schemadiff/App.java
+++ schemadiff/src/main/java/in/fourbits/schemadiff/App.java
@@ -2,12 +2,22 @@ package in.fourbits.schemadiff;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import difflib.Chunk;
 
 /**
@@ -23,7 +33,7 @@ public class App {
 	private static String currentFileName;
 	private static String revisedFileExtension;
 	private static String revisedFileURL;
-	
+
 	/**
 	 * Initialize fields by reading from external config file
 	 */
@@ -45,8 +55,55 @@ public class App {
 		}
 	}
 
+	public static void sendMail() {
+		Properties properties = new Properties();
+		FileInputStream fis;
+		final String username = "<username>";
+		final String password = "<password>";
+		String fromAddress = "";
+		String toAddress = "";
+		try {
+			fis = new FileInputStream("config.properties");
+			properties.load(fis);
+			fromAddress = properties.getProperty("mailFrom");
+			toAddress = properties.getProperty("mailTo"); // suraj.trivikram.acharya@sap.com
+			Properties props = new Properties();
+			
+			props.put("mail.smtp.auth", properties.getProperty("mailSmtpAuth"));
+			props.put("mail.smtp.starttls.enable", properties.getProperty("mailSmtpTlsEnabled"));
+			props.put("mail.smtp.host", properties.getProperty("mailSmtpHost"));
+			props.put("mail.smtp.port", properties.getProperty("mailSmtpPort"));
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+
+			try {
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(fromAddress));
+				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toAddress));
+				message.setSubject(properties.getProperty("mailSubject"));
+				message.setText(properties.getProperty("mailBody"));
+
+				Transport.send(message);
+
+				System.out.println("Schema difference Notification sent to the team");
+
+			} catch (MessagingException e) {
+				System.out.println("Schema difference Notification to team failed");
+				throw new RuntimeException(e);
+			}
+
+		} catch (IOException e1) {
+			System.out.println("Schema difference Notification to team failed");
+			e1.printStackTrace();
+		}
+	}
+
 	/**
 	 * Main method
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -55,23 +112,26 @@ public class App {
 
 		new SchemaDownloader().downloadSchema(revisedFileURL, revisedFilePath, revisedFileExtension);
 		System.out.println("Downloading revised file from : " + revisedFileURL);
-		
+
 		try {
 			Thread.sleep(30000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		DateFormat formatter = new SimpleDateFormat("ddMMyyy");
 		File original = new File(currentFilePath + currentFileName);
 		File revised = new File(revisedFilePath + formatter.format(new Date()) + revisedFileExtension);
-		
+		boolean hasChanges = false, hasInsertion = false, hasDeletion = false;
+
 		SchemaComparator comparator = new SchemaComparator(original, revised);
 		try {
 			System.out.println("\n============ Changes ===============\n");
 			List<LineDiff> diffList = comparator.getChangesFromOriginal();
+			int size = 0;
 			for (LineDiff diff : diffList) {
-				for (int i = 0; i < diff.getBefore().getLines().size(); i++) {
+				hasChanges = (size = diff.getBefore().getLines().size()) > 0 ? true : false;
+				for (int i = 0; i < size; i++) {
 					System.out.println("[Pos: " + diff.getBefore().getPosition() + "]" + getData(diff.getBefore(), i)
 							+ " --> " + getData(diff.getAfter(), i));
 				}
@@ -79,7 +139,8 @@ public class App {
 			System.out.println("\n============ Inserts ===============\n");
 			diffList = comparator.getInsertsFromOriginal();
 			for (LineDiff diff : diffList) {
-				for (int i = 0; i < diff.getBefore().getLines().size(); i++) {
+				hasInsertion = (size = diff.getBefore().getLines().size()) > 0 ? true : false;
+				for (int i = 0; i < size; i++) {
 					System.out.println("[Pos: " + diff.getBefore().getPosition() + "]" + getData(diff.getBefore(), i)
 							+ " --> " + getData(diff.getAfter(), i));
 				}
@@ -88,11 +149,19 @@ public class App {
 			System.out.println("\n============ Deletion ===============\n");
 			diffList = comparator.getDeletionFromOriginal();
 			for (LineDiff diff : diffList) {
-				for (int i = 0; i < diff.getBefore().getLines().size(); i++) {
+				hasDeletion = (size = diff.getBefore().getLines().size()) > 0 ? true : false;
+				for (int i = 0; i < size; i++) {
 					System.out.println("[Pos: " + diff.getBefore().getPosition() + "]" + getData(diff.getBefore(), i)
 							+ " --> " + getData(diff.getAfter(), i));
 				}
 			}
+
+			if (hasChanges || hasInsertion || hasDeletion) {
+				sendMail();
+			} else {
+				System.out.println("No Schema difference identified");
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -101,8 +170,11 @@ public class App {
 
 	/**
 	 * Get differences in lines
-	 * @param data can be before/after
-	 * @param i arrayList index
+	 * 
+	 * @param data
+	 *            can be before/after
+	 * @param i
+	 *            arrayList index
 	 * @return Object
 	 */
 	public static Object getData(Chunk data, int i) {
